a1226a9ba0609e728ef348bf756df0287ff9e622
mahesh <gangurdemahesh9@gmail.com>
2022-11-21 18:36:19 +0530

    [mahesh]add all file include UC 13 using Lambda Function

---
+++ Day-24-Practice-UserRegistration-Uing-Lambda/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Linq.Expressions;
+
+namespace Day_24_Practice_UserRegistration_Uing_Lambda
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 24 UserRegistration using  LambdaExpression!");
+            UserRegistration_Lambda userRegistration_Lambda = new UserRegistration_Lambda();
+            userRegistration_Lambda.Validate();
+        }
+    }
+}
---
+++ Day-24-Practice-UserRegistration-Uing-Lambda/UserRegistration_Lambda.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_24_Practice_UserRegistration_Uing_Lambda
+{
+    public class UserRegistration_Lambda
+    {
+        public void Validate()
+        {
+            string[] RegexPattern = new string[6];
+
+            //For First name
+            RegexPattern[0] = "^[A-Z]{1}[a-z]{2}[a-z]*";
+
+            //For Last Name
+            RegexPattern[1] = "^[A-Z]{1}[a-z]{2}[a-z]*";
+
+            //For Email id
+            RegexPattern[2] = "[a-zA-Z0-9_\\-\\.]+[@][a-z]+[\\.][a-z]{2,3}";
+
+            //For Phone Number
+            RegexPattern[3] = "^[9]{1}[1]{1}[ ]{1}[6-9]{1}[0-9]{9}?";
+
+            //For Password
+            RegexPattern[4] = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+
+            string[] UserInput = new string[6];
+
+            //Geting User in
+            Console.WriteLine("Enter First Name:");
+            UserInput[0] = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name:");
+            UserInput[1] = Console.ReadLine();
+
+            Console.WriteLine("Enter Email Id:");
+            UserInput[2] = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number:");
+            UserInput[3] = Console.ReadLine();
+
+            Console.WriteLine("Enter passward:");
+            UserInput[4] = Console.ReadLine();
+
+            Console.WriteLine("\nResults are: ");
+            //Lambda Expression
+            for (int i = 0; i < 5; i++)
+            {
+                Func<string, bool> nameValidater = UserInput => Regex.IsMatch(UserInput, RegexPattern[i]);
+                bool result = nameValidater(UserInput[i]);
+                Console.WriteLine(($"{i + 1} {UserInput[i]} : {result}"));
+
+            }
+
+        }
+    }
+}
